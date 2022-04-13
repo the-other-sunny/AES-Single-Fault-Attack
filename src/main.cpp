14ac978148ea64d69c996aa80761318c76eb87f1
the-same-other-sunny <99849452+the-same-other-sunny@users.noreply.github.com>
2022-04-13 18:58:32 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 
-// -----------------------------------------------------------------------
 FlatState string_to_state(string const& s) {
     FlatState X;
     
@@ -33,9 +32,8 @@ ostream& operator<<(ostream& os, FlatState const& state) {
 
     return os;
 }
-// -----------------------------------------------------------------------
 
-void crack(string const& plaintext, string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position) {
+void crack(string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position, string const& plaintext) {
     auto X = string_to_state(plaintext);
     auto Y = string_to_state(regular_ciphertext);
     auto Y_ = string_to_state(faulted_ciphertext);
@@ -48,18 +46,33 @@ void crack(string const& plaintext, string const& regular_ciphertext, string con
         cout << key << endl;
 }
 
+void crack(string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position) {
+    auto Y = string_to_state(regular_ciphertext);
+    auto Y_ = string_to_state(faulted_ciphertext);
+
+    auto stage1_results = first_stage::reduction(Y, Y_, fault_position);
+    auto stage2_results = second_stage::reduction(Y, Y_, fault_position, stage1_results);
+
+    for (auto const& K10 : stage2_results)
+        cout << get_initial_key(K10) << endl;
+}
+
 int main(int argc, char* argv[]) {
     string plaintext, regular_ciphertext, faulted_ciphertext;
     size_t fault_position;
 
-    // brace yourself, very unsafe code ahead
+    istringstream(argv[1]) >> regular_ciphertext;
+    istringstream(argv[2]) >> faulted_ciphertext;
+    istringstream(argv[3]) >> fault_position;
 
-    istringstream(argv[1]) >> plaintext;
-    istringstream(argv[2]) >> regular_ciphertext;
-    istringstream(argv[3]) >> faulted_ciphertext;
-    istringstream(argv[4]) >> fault_position;
-
-    crack(plaintext, regular_ciphertext, faulted_ciphertext, fault_position);
+    if (argc == 4) {
+        crack(regular_ciphertext, faulted_ciphertext, fault_position);
+    } else if (argc == 5) {
+        istringstream(argv[4]) >> plaintext;
+        crack(regular_ciphertext, faulted_ciphertext, fault_position, plaintext);
+    } else {
+        cout << "Usage: aes-single-fault-attack regular_cipher faulted_cipher fault_position [plaintext]" << endl;
+    }
 
     return 0;
 };
