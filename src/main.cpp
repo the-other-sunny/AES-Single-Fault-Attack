f1e8e9a877bd814dbf3a58a0193325d27675ce0b
the-same-other-sunny <99849452+the-same-other-sunny@users.noreply.github.com>
2022-04-13 19:20:20 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,46 +33,41 @@ ostream& operator<<(ostream& os, FlatState const& state) {
     return os;
 }
 
-void crack(string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position, string const& plaintext) {
-    auto X = string_to_state(plaintext);
-    auto Y = string_to_state(regular_ciphertext);
-    auto Y_ = string_to_state(faulted_ciphertext);
-
-    auto stage1_results = first_stage::reduction(Y, Y_, fault_position);
-    auto stage2_results = second_stage::reduction(Y, Y_, fault_position, stage1_results);
-    auto stage3_results = third_stage::reduction(Y, X, stage2_results);
-
-    for (auto const& key : stage3_results)
-        cout << key << endl;
-}
-
-void crack(string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position) {
+void crack(string const& regular_ciphertext, string const& faulted_ciphertext, size_t fault_position, string const& plaintext = "") {
     auto Y = string_to_state(regular_ciphertext);
     auto Y_ = string_to_state(faulted_ciphertext);
 
     auto stage1_results = first_stage::reduction(Y, Y_, fault_position);
     auto stage2_results = second_stage::reduction(Y, Y_, fault_position, stage1_results);
 
-    for (auto const& K10 : stage2_results)
-        cout << get_initial_key(K10) << endl;
+    if (plaintext == "") {
+        for (auto const& key : stage2_results)
+            cout << key << endl;
+    } else {
+        auto X = string_to_state(plaintext);
+        auto stage3_results = third_stage::reduction(Y, X, stage2_results);
+
+        for (auto const& key : stage3_results)
+            cout << key << endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
     string plaintext, regular_ciphertext, faulted_ciphertext;
     size_t fault_position;
 
+    if (argc != 4 || argc != 5) {
+        cout << "Usage: aes-single-fault-attack regular_cipher faulted_cipher fault_position [plaintext]" << endl;
+        return 1;
+    }
+
     istringstream(argv[1]) >> regular_ciphertext;
     istringstream(argv[2]) >> faulted_ciphertext;
     istringstream(argv[3]) >> fault_position;
-
-    if (argc == 4) {
-        crack(regular_ciphertext, faulted_ciphertext, fault_position);
-    } else if (argc == 5) {
-        istringstream(argv[4]) >> plaintext;
-        crack(regular_ciphertext, faulted_ciphertext, fault_position, plaintext);
-    } else {
-        cout << "Usage: aes-single-fault-attack regular_cipher faulted_cipher fault_position [plaintext]" << endl;
-    }
+    if (argc == 5) istringstream(argv[4]) >> plaintext;
+    
+    // TODO: add checks to sanitize and validate input
+    crack(regular_ciphertext, faulted_ciphertext, fault_position, plaintext);
 
     return 0;
 };
